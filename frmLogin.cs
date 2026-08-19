72780f63ef4940abd7b8f46fef27ae938854e70d
Adib21-dot <radib355@gmail.com>
2026-08-20 00:28:07 +0600

    Add files via upload

---
+++ frmLogin.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+namespace Login_and_Register
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(
+                    ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString);
+
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string login = "SELECT * FROM tbl_users WHERE Username = @username AND Password = @password";
+
+            cmd = new SqlCommand(login, con);
+
+            cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read() == true)
+            {
+                new frmDashboard().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Username and Password, Please Try Again",
+                    "Login Failed",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                txtUsername.Text = "";
+                txtPassword.Text = "";
+                txtUsername.Focus();
+            }
+
+            con.Close();
+        }
+
+        private void checkbxShowPas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkbxShowPas.Checked)
+            {
+                txtPassword.PasswordChar = '\0';             
+            }
+            else
+            {
+                txtPassword.PasswordChar = '•';
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtUsername.Focus();
+        }
+
+        private void clickRegister_Click(object sender, EventArgs e)
+        {
+            new frmRegister().Show();
+            this.Hide();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye");
+            Application.Exit();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
